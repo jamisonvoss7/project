60f3a1c0539e65f5e0d98014ebef26aef87a7782
Jamison Voss <jamison.voss@icloud.com>
2016-07-12 14:48:47 -0500

    got the signup/sign in flow to work with new Firebase sdk.

---
+++ TailgateMate/TailgateMate/Source/Domain/Contact.h
@@ -0,0 +1,13 @@
+//
+//  Contact.h
+//  TailgateMate
+//
+//  Created by Jamison Voss on 7/3/16.
+//  Copyright © 2016 Jamison Voss. All rights reserved.
+//
+
+#import "FirebaseObject.h"
+
+@interface Contact : FirebaseObject
+
+@end
---
+++ TailgateMate/TailgateMate/Source/Domain/Contact.m
@@ -0,0 +1,13 @@
+//
+//  Contact.m
+//  TailgateMate
+//
+//  Created by Jamison Voss on 7/3/16.
+//  Copyright © 2016 Jamison Voss. All rights reserved.
+//
+
+#import "Contact.h"
+
+@implementation Contact
+
+@end
